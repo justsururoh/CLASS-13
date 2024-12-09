e7e5c89740440a4c56338e5fa13eaa6987352007
Muhammed Sururat Omotoyosi <sururatomotoyosi@gmail.com>
2024-12-09 11:38:30 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,79 @@
+let count = 6
+
+console.log(count);
+
+console.log(++count);
+
+console.log(count + 4);
+
+let animal = 3
+let food = 6
+
+console.log(animal + food);
+console.log(animal > food);
+
+let name = "lara"
+console.log(name);
+
+
+let myname = "abc"
+
+if (myname == "abc") {
+    console.log("that's my name"); 
+} else {
+    console.log("Error"); 
+}
+
+
+let a = 10;
+
+if (a > 20) {
+   console.log(" a above 20");
+} else { console.log("a below 20");   
+}
+
+
+
+let score = 70;
+
+if (score >= 70) {
+    console.log("A");  
+} else if (score >= 60) 
+    { console.log("B");
+} else if (score >= 50) 
+    { console.log("C");
+} else if (score >= 40) 
+    { console.log("D");
+} else if (score >= 30) 
+    { console.log("F9");
+} else if (score >= 0) 
+    { console.log("That's not a score");
+} 
+
+
+
+let myscore = 60;
+
+if (myscore >= 60 ) {
+    console.log("Grade B"); 
+} else { console.log("F9");
+}
+let yscore = 50;
+
+if (yscore >= 50 ) {
+    console.log("Grade C"); 
+} else { console.log("F9");
+}
+let mscore = 40;
+
+if (mscore >= 40 ) {
+    console.log("Grade D"); 
+} else { console.log("F9");
+}
+let OURscore = 30;
+
+if (OURscore >= 30 ) {
+    console.log("F9"); 
+} else { console.log("F9");
+}
+
